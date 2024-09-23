58a36aadd99b78c6b1c5e6ba665bbf79838701a9
Janithya Dias <it21203176@my.sliit.lk>
2024-09-23 20:34:30 +0530

    Checkout Activity Commit

---
+++ app/src/main/java/com/example/ecommerce_mobile_app/CheckoutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ecommerce_mobile_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
