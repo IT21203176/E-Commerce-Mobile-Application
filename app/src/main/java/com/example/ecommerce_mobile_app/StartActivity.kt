fd21f14dd34a5b3fb183c8ecba05fc8fdac2fa8f
Janithya Dias <it21203176@my.sliit.lk>
2024-09-24 15:38:02 +0530

    Start to Login Nav Commit

--- app/src/main/java/com/example/ecommerce_mobile_app/StartActivity.kt
+++ app/src/main/java/com/example/ecommerce_mobile_app/StartActivity.kt
@@ -1,20 +1,28 @@
 package com.example.ecommerce_mobile_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce_mobile_app.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.getStartBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
