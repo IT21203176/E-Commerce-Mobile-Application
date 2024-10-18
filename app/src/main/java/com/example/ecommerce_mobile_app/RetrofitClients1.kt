23250ae11fdfc46978a720800f91ad93fb2cc3dc
Janithya Dias <it21203176@my.sliit.lk>
2024-10-18 15:55:29 +0530

    Product Updated Commit

---
+++ app/src/main/java/com/example/ecommerce_mobile_app/RetrofitClients1.kt
@@ -0,0 +1,31 @@
+package com.example.ecommerce_mobile_app
+
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitClients1 {
+    private const val BASE_URL ="https://localhost:8083/api/"
+
+    private val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val apiService : ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
+    }
+}
